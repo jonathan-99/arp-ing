296e30a82567299ba3cdc4128ff4a6bb0893feb8
local_admin <t@h.com>
2022-08-19 16:48:26 +0100

    Adding a testing function

---
+++ arp-testing.py
@@ -0,0 +1,19 @@
+import unittest
+import 'arp-v' as tool
+
+
+class MyTestCase(unittest.TestCase):
+    def test_ping_success(self):
+        output = tool.run_command("ping 127.0.0.1")
+        success = "(0% loss"
+        self.assertIn(success, output)
+
+
+    def test_ping_fail(self):
+        output = tool.run_command("this should fail")
+        success = "00% loss"
+        self.assertIn(success, output)
+
+
+if __name__ == '__main__':
+    unittest.main()
